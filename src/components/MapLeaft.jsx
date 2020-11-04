ff3b7c506a64563ed8370a093580ffb5039667af
jordandev <rjaramilloby@gmail.com>
2020-11-04 10:50:15 -0500

    Marker agregado al mapa, transicion en locacion !

--- src/components/MapLeaft.jsx
+++ src/components/MapLeaft.jsx
@@ -1,22 +1,30 @@
-import { MapContainer, TileLayer } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+// import "leaflet/dist/leaflet.css";
 import "./MapLeaft.css";
 import { useState, useEffect } from "react";
 const MapLeaft = ({ mapCenter }) => {
   const [map, setMap] = useState(null);
   useEffect(() => {
     if (map) {
-      map.setView(mapCenter, 15);
+      // map.setView(mapCenter, 15);
+      map.flyTo(mapCenter, map.getZoom()); //animacion
     }
   }, [mapCenter]);
-  console.log(mapCenter);
   return (
     <div className="map">
-      <MapContainer center={mapCenter} zoom={15} whenCreated={setMap}>
+      <MapContainer
+        center={mapCenter}
+        scrollWheelZoom={false}
+        zoom={15}
+        whenCreated={setMap}
+      >
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
+        <Marker position={mapCenter}>
+          <Popup>You are here</Popup>
+        </Marker>
       </MapContainer>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import "./Navbar.css";
 const Navbar = () => {
   return (
-    <nav>
-      <p>Places</p>
+    <nav className="nav">
+      <p className="nav__brand">Places</p>
       <div className="nav__search">
-        <input type="text" placeholder="Buscar lugar" />
-        <img src13="buscar.svg" className="search-icon" alt="Buscar icono" />
+        <input type="text" placeholder="Buscar lugar" className="nav__input" />
+        <img src="buscar.svg" className="search-icon" alt="Buscar icono" />
       </div>
       <div className="nav__icons"></div>
     </nav>
